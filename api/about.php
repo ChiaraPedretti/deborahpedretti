67140c1bd53bd28dd42d8ab5f62635f4c28171a5
Chiara Pedretti <cpdesign.ch@gmail.com>
2025-02-06 22:47:15 +0100

    Add DM Sans font import and update font styles for consistency

--- api/about.php
+++ api/about.php
@@ -19,6 +19,7 @@
     <!-- FINE SMOOTH SCROLL -->
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap');
     </style>
 </head>
 
